cb56a932550016ca0c3d54be80d23ba327dab9c0
dinithrathnayaka23 <rathnayakarmdno.23@uom.lk>
2026-07-17 11:55:50 +0530

    Implemented Spinner Component

--- MovieHub/src/App.jsx
+++ MovieHub/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   const [errorMessage,setErrorMessage]= useState('');
   const [movieList,setMovieList]= useState([]);
-  const [isLoading,setIsLoading]= useState(true);
+  const [isLoading,setIsLoading]= useState(false);
 
   const fetchMovies=async () =>{
     setIsLoading(true);
@@ -63,9 +63,9 @@ const App = () => {
           </header>
 
           <section className="all-movies">
-              <h2>All Movies</h2>
+              <h2 className="mt-[40px]">All Movies</h2>
               {isLoading ? (
-                <p className="text-white">Loading...</p>
+                <Spinner></Spinner>
               ):errorMessage ? (
                 <p className="text-red-500">{errorMessage}</p>
               ) :(
