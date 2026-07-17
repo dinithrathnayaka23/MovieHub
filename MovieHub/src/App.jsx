015299291500926c89bcacd6e913548e9d48b8b1
dinithrathnayaka23 <rathnayakarmdno.23@uom.lk>
2026-07-17 10:11:29 +0530

    Implemented the Search bar component

--- MovieHub/src/App.jsx
+++ MovieHub/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+  const[searchTerm,setSearchTerm]=useState('');
+
   return (
     <main>
         <div className="pattern"/>
         <div className="wrapper">
           <header>
-            <h1>Find <span className="text-gradient">Movies</span> You will Enjoy without the Hussle !</h1>
+            <img src='./hero.png' alt='Hero Image'></img>
+            <h1>Find <span className="text-gradient">Movies</span> You will Enjoy Welcome to MovieHub !</h1>
           </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}></Search>
         </div>
         
     </main>
